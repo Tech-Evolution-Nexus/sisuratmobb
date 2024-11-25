9cec55938f7ae8a3b8a8d1fa7121938e8384e1d6
nonamee12 <kurogane.ikki1223@gmail.com>
2024-11-25 09:07:00 +0700

    add profile

--- app/src/main/java/com/nixie/sisuratmob/Api/ApiClient.java
+++ app/src/main/java/com/nixie/sisuratmob/Api/ApiClient.java
@@ -5,7 +5,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.1.7/SISURAT/api/"; // Ganti dengan URL API Anda
+    private static final String BASE_URL = "http://192.168.93.122/SISURAT/api/"; // Ganti dengan URL API Anda
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofitInstance() {
